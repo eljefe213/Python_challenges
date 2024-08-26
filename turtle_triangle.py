d19571eaae4969bb626a1083123776ec374d21ac
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2024-08-26 16:37:18 +0200

    Create turtle_triangle.py

---
+++ turtle_triangle.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+color('purple') # Set color to Purple
+forward(200) 
+left(120)
+color('blue') # Set color to Blue
+forward(200) 
+color('magenta') # Set color to Magenta
+left(120)
+color('red') # Set color to Red
+forward(200) # Left side of triangle
+
+bye()
