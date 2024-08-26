9558506a892609fbbbe8695651cb8eaa1f9b1b63
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2024-08-26 17:03:19 +0200

    Update turtle_letterC.py

--- turtle_letterC.py
+++ turtle_letterC.py
@@ -15,3 +15,18 @@ forward(50)
 
 hideturtle()
 bye()
+
+
+# The same program with the for loop 
+
+from turtle import *
+width(5)
+
+left(180)
+
+# Start of for loop.
+for i in range(5):
+    forward(50) 
+    left(45) 
+hideturtle()
+bye()
