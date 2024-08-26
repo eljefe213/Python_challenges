ba55ef2f7ce3f767ec58147f9cb819eb6876fc57
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2024-08-26 17:01:28 +0200

    Create turtle_letterC.py

---
+++ turtle_letterC.py
@@ -0,0 +1,17 @@
+from turtle import *
+width(5)
+
+left(180)
+
+forward(50)
+left(45)    # Turning angle
+forward(50) 
+left(45)    # Turning angle
+forward(50)
+left(45)    # Turning angle
+forward(50) 
+left(45)    # Turning angle
+forward(50) 
+
+hideturtle()
+bye()
