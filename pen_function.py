656e2a6d675eb0e27e329599480cd1339cb68d65
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2024-08-26 16:59:06 +0200

    Create pen_function.py

---
+++ pen_function.py
@@ -0,0 +1,10 @@
+from turtle import *
+
+forward(100)
+penup() # Lift the pen
+forward(100) # Move forward 100 px without drawing.
+left(90) # Turn left 90 degrees
+pendown() # Lower the pen again.
+forward(100) # Move forward 100 px while drawing
+
+bye()
