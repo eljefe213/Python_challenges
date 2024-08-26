204b0d8736cdfa62313a8880cdb45b54d23be35e
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2024-08-26 16:24:39 +0200

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,13 @@
+from turtle import * # Loads commands used to draw
+
+color('red') 
+forward(200) # Moves the turtle 200 pixels right
+left(90) # Turn left 90 degrees
+# Turtle is now facing up
+forward(200) 
+left(90) 
+forward(200)
+left(90) 
+forward(200)
+
+bye() # Tells the turtle we're done writing commands
