358e9aed5c66cb733a01bd0d066ed1d0c600acc1
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2024-08-28 17:18:13 +0200

    Create find.py

---
+++ find.py
@@ -0,0 +1,47 @@
+# find function
+
+#Exercice 1 : 
+str1 = "parts"
+str2 = "art"
+print(str1.find(str2))
+
+#Exercice 2 : 
+str1 = "parts"
+str2 = "art"
+print(str1.find(str2))
+
+#Exercice 3 :
+str1 = "Is it going well?" 
+str2 = "go"
+print(str1.find(str2))
+
+#Exercice 4 : 
+str1 = "Is it going well?" 
+str2 = "go"
+print(str1.find(str2))
+
+#Exercice 5 :
+str1 = "superior"
+str2 = "Perfect"
+str3 = "personally"
+str4 = "miles per gallon"
+
+print(str1.find("per"))
+print(str2.find("per"))
+print(str3.find("per"))
+print(str4.find("per"))
+
+#Exercice 6 : 
+str1 = "superior"
+str2 = "Perfect"
+str3 = "personally"
+str4 = "miles per gallon"
+
+print(str1.find("per"))
+print(str2.find("per"))
+print(str3.find("per"))
+print(str4.find("per"))
+
+#Exercice 7 :
+test_string = "tested, testing, tests, test"
+print(test_string.find("test")) 
